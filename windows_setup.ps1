771104ab5b4f04f273f4f64e9407f65c4abe7126
Saar Sayfan <saarsayfan@gmail.com>
2018-02-24 16:03:03 -0800

    Added windows_setup.ps1

---
+++ windows_setup.ps1
@@ -0,0 +1,11 @@
+choco install -y python2 `
+                 python `
+                 jetbrainstoolbox `
+                 git `
+                 adobereader `
+                 jre8 `
+                 nodejs `
+                 atom `
+                 yarn `
+                 sqlite
+pip install pipenv
