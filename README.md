dbf9b6aca588abfb084d084e55e51ae04d5aea57
Saar Sayfan <saarsayfan@gmail.com>
2025-01-21 00:11:26 -0800

    Started modernization - revamped windows setup script and README

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # dotfiles
-Various Unix dotfiles
+Dotfiles and scripts to set up my environment across different systems.
 
-Everything starts from .bashrc
-http://superuser.com/questions/49289/what-is-the-bashrc-file
+## Windows
+Run the `setup.ps1` script as follows:
+```
+# Temporarily bypass the default execution policy which prevents running
+# powershell scripts for the current powershell session.
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+.\setup.ps1
+```
+
+This script will install the packages in `win_packages.txt` (via [`winget`](https://learn.microsoft.com/en-us/windows/package-manager/winget/))
+and set up WSL2 with the default distro (Ubuntu).
+
---
+++ setup.ps1
@@ -0,0 +1,54 @@
+# Set up params
+param(
+    [Alias("d")][switch]$dryRun,
+    [Alias("v")][switch]$verbose
+)
+
+if ($dryRun) {
+    Write-Host "Dry run mode. No operations will be performed." -ForegroundColor Red
+}
+
+Write-Host "Setting up Windows environment..." -ForegroundColor Yellow
+
+# Step 1: Install packages
+Write-Host "1. Installing Windows packages..." -ForegroundColor Yellow
+
+# Read the package names, ignoring comments and empty lines
+$packagesFile = ".\win_packages.txt"
+$packageNames = Get-Content $packagesFile | Where-Object { 
+    $_.Trim() -ne '' -and $_.Trim() -notmatch '^\s*#' 
+}
+
+foreach ($packageName in $packageNames) {
+    Write-Host "  Installing" -NoNewline
+    Write-Host " $packageName" -ForegroundColor Blue
+
+    $wingetCommand = "winget install --id $packageName --source winget -e --accept-source-agreements --accept-package-agreements"
+
+    # Print out the command being run in verbose mode
+    if ($verbose) {
+        Write-Host "    $wingetCommand" -ForegroundColor DarkGray
+    } 
+
+    if ($dryRun) {
+        continue
+    }
+
+    # Run the command
+    try {
+        Invoke-Expression $wingetCommand
+        Write-Host "  $packageName installation completed."
+    } catch {
+        Write-Host "  Failed to install $packageName. Skipping..."
+    }
+}
+
+Write-Host "Packages installation complete!" -ForegroundColor Green
+
+# Step 2: Enable WSL
+Write-Host "Enabling WSL feature..." -ForegroundColor Yellow
+
+wsl --install
+wsl --set-default-version 2
+
+Write-Host "WSL enabled!" -ForegroundColor Green
